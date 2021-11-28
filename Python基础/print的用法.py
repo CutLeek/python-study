08fe1f9ee9b9018604ec96b5f98a89ab012f84a4
CutLeek <kirobot@163.com>
2021-11-28 11:02:34 +0800

    how to print

---
+++ Python基础/print的用法.py
@@ -0,0 +1,27 @@
+#变量的赋值
+test1=10
+test2=10
+
+#交换test1和test2的值
+test1,test2=test2,test1
+print(test1,test2)
+
+#print输出普通字符串
+print("hello world")
+
+#print输出变量
+a=10
+print(a)
+
+#print中的三引号，完美还原原来的格式
+print("""
+this is the first line
+this is the "second" line
+""")
+
+#格式化的输出
+print("变量a的值是:%s,变量test1的值是:%s" %(a,test1))
+
+
+
+
