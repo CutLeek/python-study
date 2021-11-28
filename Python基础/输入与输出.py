fe141ba6c206a2932321a6df1a7693f0fdeaae59
CutLeek <kirobot@163.com>
2021-11-28 13:57:18 +0800

    update print and input

--- Python基础/输入与输出.py
+++ Python基础/输入与输出.py
@@ -1,6 +1,6 @@
 #变量的赋值
 test1=10
-test2=10
+test2=20
 
 #交换test1和test2的值
 test1,test2=test2,test1
@@ -10,7 +10,7 @@ print(test1,test2)
 print("hello world")
 
 #print输出变量
-a=10
+a=30
 print(a)
 
 #print中的三引号，完美还原原来的格式
@@ -19,9 +19,12 @@ this is the first line
 this is the "second" line
 """)
 
+
 #格式化的输出
-print("变量a的值是:%s,变量test1的值是:%s" %(a,test1))
+print("变量a的值是:%s,变量test1的值是:%s" % (a,test1))
+print("{}比9大".format(a))
+print("{1}比1大，{0}比20小".format(test1,test2))
 
-
-
-
+#读取键盘的输入
+ToEat=input("今晚吃什么：")
+print(ToEat)
