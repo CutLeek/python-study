cf4552a45f0125180bb11e34fa15882ca6b3f444
CutLeek <kirobot@163.com>
2021-11-29 20:51:05 +0800

    if and for and while

--- Python基础/判断与循环.py
+++ Python基础/判断与循环.py
@@ -1,3 +1,11 @@
+import random
+
+c = random.randint(1,10)
+print("c的值是{}".format(c))
+d = random.randrange(0,100,20)
+print("d的值是{}".format(d),type(d))
+
+
 #if判断
 a = 3
 b = 20
@@ -8,5 +16,10 @@ else:
 
 
 #for循环
+#range左闭右开的区间,range(1,10)代表1到9，range(10)代表0到9
+for i in range(1,10):
+    c = random.randint(1, 10)
+    print(i,c)
+
 
 #while循环
