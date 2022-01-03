062909022227b3a65980d70d5a8fbdf8e4793efd
CutLeek <kirobot@163.com>
2022-01-03 08:19:55 +0800

    update a file

---
+++ Python基础/函数/函数.py
@@ -0,0 +1,9 @@
+def login(username,passswd):
+    use="test"
+    password=123
+    if username==use and password==passswd:
+        print("登陆成功")
+username=input("输入用户名：")
+passswd=int(input("输入密码："))
+login(username,passswd)
+
