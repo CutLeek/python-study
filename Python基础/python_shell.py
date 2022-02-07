6e6df5ab579f2a6f55091d19f093cff985e782b6
CutLeek <kirobot@163.com>
2022-02-07 17:01:36 +0800

    update a file

---
+++ Python基础/python_shell.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+import subprocess
+subprocess.Popen('df -Th',shell=True)
+
---
+++ Python基础/日志.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import logging
+
+LOG_FORMAT = "%(asctime)s [%(levelname)s]  %(message)s"          #配置输出日志格式
+DATE_FORMAT = '%Y-%m-%d  %H:%M:%S'                                               #配置输出时间的格式，注意月份和天数不要搞乱了
+logging.basicConfig(level=logging.DEBUG,
+                    format=LOG_FORMAT,
+                    datefmt = DATE_FORMAT ,
+                    filename="/tmp/test.log"                                     #有了filename参数就不会直接输出显示到控制台，而是直接写入文件
+                    )
+logging.debug("debug_msg")
+logging.info("info_msg")
+logging.warning("warning_msg")
+logging.error("error_msg")
+logging.critical("critical_msg")
+logging.error(LOG_FORMAT)
