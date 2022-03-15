a5ad3c8e72d49492dcbc47ee6f7f9b92d09739d4
CutLeek <kirobot@163.com>
2022-03-16 07:17:07 +0800

    update a file

--- Python基础/函数/嵌套函数.py
+++ Python基础/函数/嵌套函数.py
@@ -24,6 +24,29 @@ x()
 s = lambda a,b:a+b
 print(s(1,2))
 
+#对列表里的奇数进行加1操作,map就是用来对列表里元素进行操作的，不改变源列表，返回列表
+list1=[1,2,3,4,5,6,7,8]
+result = map(lambda x:x if x % 2 ==0 else x+1,list1)
+print(list1)
+print(list(result))
+
+#reduce 对列表中的元素进行加减乘除操作，返回操作结果
+from functools import reduce
+tuple1=(3,5,7,8,9)
+result=reduce(lambda x,y:x+y,tuple1)
+print(result)
+
+#filter,对列表中的元素进行过滤，不改变原列表
+list2=[12,6,8,95,98,34,36,2,8]
+result=filter(lambda x:x>10,list2)
+print(list(result))
+
+#sorted 排序，重点就是可以指定key
+result = sorted(list2)
+print(result)
+
+#-------------------------------------------------------递归函数-------------------------------------------------
+#特点：我自己调用我自己、
 
 
 
