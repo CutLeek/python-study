fcdad46b3f5333b4e7a55150930830e72dae4ff9
CutLeek <kirobot@163.com>
2022-03-23 08:29:52 +0800

    update a file

--- Python基础/面向对象.py
+++ Python基础/面向对象.py
@@ -22,15 +22,16 @@ print(zhangSan.high, '-----------------------------------------')
 
 # 方法(普通方法，类方法，静态方法，魔术方法)
 class Student:
-    def DoHomeWork(self):
+    def DoHomeWork(self):  # self就是我的对象
         print(self)
         print(self.pen)
 
 
 lisi = Student()
-print(lisi)
+print('lisi是：', lisi)
 lisi.pen = 'pen'
 lisi.DoHomeWork()
+print('---------------------------------')
 
 
 # 构造器和__init__方法
@@ -41,6 +42,46 @@ class Student:
         print('----------------------init--------------------')
 
 
-wangwu = Student('hhhhhhhddddddddd')
+wangwu = Student('hhhhhhhddddddddd')  # 这个值就是传给__init__了，pen=‘hhhhhdddd’，self.pen=pen,所以wangwu.pen=pen='hhhhhhdddd'
+print(wangwu.pen)
 
 
+# 类方法
+class Dog:
+    def __init__(self, name):
+        self.name = name
+
+    def run(self):  # 这个方法只有对象才能去调用
+        print('{}在院子里跑'.format(self.name))
+
+    @classmethod  # 加了装饰器就是类方法
+    def test(cls):  # 这个cls就是我的类
+        a = cls('aa')
+        a.run()
+
+
+Dog.test()  # 创建对象之前，就可以去调用类方法
+d = Dog('大黄')
+d.run()
+d.test()
+
+
+# 类的私有变量
+class Person:
+    __age = 18  # 在变量名前边加__就变成私有变量了，在外面没办法改
+
+    def show(self):
+        print(self.__age)
+
+    @classmethod
+    def update_age(cls):
+        cls.__age += 1
+        print('current age is {}'.format(cls.__age))
+
+    @staticmethod  # 加了这个就是静态方法，静态方法也是不依赖对象的，可以直接拿类名去调用
+    def test():
+        print("I'm staticmethod")
+
+
+Person.update_age()
+Person.test()
